adfc8d4cef18df784962defb0a60a38c7e84633f
yurongshui <yurongshui@163.com>
2017-10-11 16:21:02 +0800

    安达市多

--- MainActivity.java
+++ MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+		是的范德萨发顺丰；
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
